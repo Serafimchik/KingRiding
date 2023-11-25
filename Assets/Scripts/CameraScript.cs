2d6f0dd8153eb7acf4e0888b8c6fe1de55a6406d
ThisisFinee <108457032+ThisisFinee@users.noreply.github.com>
2023-11-25 16:11:51 +0300

    offset error

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,7 +11,7 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private float smoothing = 3f;
     void Start()
     {
-
+        offset.z = -10;
     }
 
     // Update is called once per frame
