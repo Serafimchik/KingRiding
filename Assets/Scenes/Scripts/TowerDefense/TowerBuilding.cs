2bb646488653295f9acf1cdee40d6d97c0f788be
ThisisFinee <108457032+ThisisFinee@users.noreply.github.com>
2024-06-19 18:44:12 +0300

    Maps and building scripts (#30)
    * Create td_maps and scripts for tower building
    
    * Icons final
    
    * maps end

--- Assets/Scenes/Scripts/TowerDefense/TowerBuilding.cs
+++ Assets/Scenes/Scripts/TowerDefense/TowerBuilding.cs
@@ -6,10 +6,11 @@ public class TowerBuilding : MonoBehaviour
 {
     [SerializeField] private Vector2 towerSize;
     [SerializeField] private new Renderer renderer;
+    [SerializeField] public int towerHigh;
 
     public Vector2 TowerSize { get => towerSize;set {; } }   
     
-    public void SetColor(bool isAllowedtoBuild)
+    private void SetColor(bool isAllowedtoBuild)
     {
         //print("Color moment");
         if (isAllowedtoBuild)
@@ -17,8 +18,29 @@ public class TowerBuilding : MonoBehaviour
         else renderer.material.color = Color.red;
     }
 
-    public void ResetColor()
+    private void ResetColor(Color color)
     {
-        renderer.material.color = Color.white;
+        renderer.material.color = color; // Color.white;
     }
+
+    public void ApplySetColor(bool isAllowedtoBuild)
+    {
+        Renderer[] towerBuildings = GetComponentsInChildren<Renderer>();
+        foreach (Renderer towerBuilding in towerBuildings) 
+        { 
+            renderer = towerBuilding;
+            SetColor(isAllowedtoBuild); 
+        }
+    }
+
+    public void ApplyResetColor(List<Color> towercolors)
+    {
+        Renderer[] towerBuildings = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < towerBuildings.Length; i++)
+        {
+            renderer = (Renderer)towerBuildings[i];
+            ResetColor(towercolors[i]);
+        }
+    }
+
 }
